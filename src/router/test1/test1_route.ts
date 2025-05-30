9e5095a33bc3e33d3f34abf287c7c4203b602880
najongjine <najongjin3@hotmail.com>
2025-05-30 10:55:26 +0900

    .

--- src/router/test1/test1_route.ts
+++ src/router/test1/test1_route.ts
@@ -5,6 +5,7 @@ import { TDummy1 } from "../../entities/TDummy1";
 const router = new Hono();
 
 router.get("/", async (c) => {
+  // 자료구조화된 객체
   let result: { success: boolean; data: any; code: string; message: string } = {
     success: true,
     data: null,
@@ -12,9 +13,13 @@ router.get("/", async (c) => {
     message: ``,
   };
   try {
+    // AppDataSource == DB   t_dummy1 테이블에 접근할 준비를 해라. 전문용어로 repository
     const dummy1Repo = AppDataSource.getRepository(TDummy1);
+    // dummy1 repository 를 사용해서 데이터 1000개 가져옴
     let data = await dummy1Repo.find({ take: 1000 });
+    // result.data 여기에 데이터 가져올걸 저장시킴
     result.data = data;
+    // 클라이언트에 보내줌
     return c.json(result);
   } catch (error: any) {
     result.success = false;
