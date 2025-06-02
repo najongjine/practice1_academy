02dccebdbb1d874dffa2b326710dec485f43e08c
najongjine <najongjin3@hotmail.com>
2025-06-02 10:13:48 +0900

    .

--- src/router/test1/test1_route.ts
+++ src/router/test1/test1_route.ts
@@ -78,18 +78,12 @@ router.post("/update", async (c) => {
     let _body: any = await c.req.json(); // JSON 형태로 body 파싱
 
     let name: string = _body?.name ?? "";
+    let idp = Number(_body?.idp) ?? 0;
     // AppDataSource == DB   t_dummy1 테이블에 접근할 준비를 해라. 전문용어로 repository
     const dummy1Repo = AppDataSource.getRepository(TDummy1);
 
-    // 클래스를 진짜 사용하기위해서 인스턴스화 함. TDummy1 클래스를 직접 봐보면 @ 것들이 붙어있음
-    // 이건 클래스를 db테이블 이랑 1:1로 연결시킨것임
-    // 여기서 new TDummy1 이건 새로운 데이터란 뜻임
-    let newDummy1 = new TDummy1();
-    newDummy1.name = name;
-    // 테이블에 데이터 저장
-    let data = await dummy1Repo.save(newDummy1);
-    // result.data 여기에 데이터 가져올걸 저장시킴
-    result.data = data;
+    let existData =
+      (await dummy1Repo.findOne({ where: { idp: idp } })) ?? new TDummy1();
     // 클라이언트에 보내줌
     return c.json(result);
   } catch (error: any) {
