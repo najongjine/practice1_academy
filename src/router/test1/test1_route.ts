8d75c73d8fef6d5f03e946223825acd496e1c4a9
najongjine <najongjin3@hotmail.com>
2025-06-03 10:32:06 +0900

    .

--- src/router/test1/test1_route.ts
+++ src/router/test1/test1_route.ts
@@ -114,11 +114,17 @@ router.post("/upsert", async (c) => {
     // AppDataSource == DB   t_dummy1 테이블에 접근할 준비를 해라. 전문용어로 repository
     const dummy1Repo = AppDataSource.getRepository(TDummy1);
 
+    // idp 가 0번인거 찾아와라
+    // 없네?
+    // new TDummy1() 요게 발동
+    // TDummy1 이 t_dummy1 테이블. new TDummy1() 이 뜻은 새로운 데이터 라는 뜻이죠
     let existData =
       (await dummy1Repo.findOne({ where: { idp: idp } })) ?? new TDummy1();
+    // idp 가 0 이상인데, DB 조회에서 데이터가 없으면 ... 이걸 수학 수식으로 표현함
     if (idp && !existData?.idp) {
       result.success = false;
       result.message = "없는 데이터를 수정하려 합니다. 반려처리 하겠습니다";
+      // return 이 들어가면 퉤 뱉고, 함수 종료
       return c.json(result);
     }
     existData.name = name;
